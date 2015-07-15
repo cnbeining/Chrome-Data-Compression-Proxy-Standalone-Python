3e1108a154d7d0b37dccabefd40b77092afe35a7
cnbeining <cnbeining@gmail.com>
2015-07-15 03:06:44 -0400

    The very start

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Chrome-Data-Compression-Proxy-Standalone-Python
-A Python wrapper of Chrome DCP.
+A Python wrapper of Chrome DCP. 
+
+Inspired by datacompressionproxy(https://code.google.com/p/datacompressionproxy/), and Sogou proxy(http://xiaoxia.org/2011/11/14/update-sogou-proxy-program-with-https-support/).
+
+Details of Chrome DCP:
+
+https://developer.chrome.com/multidevice/data-compression
+
+https://support.google.com/chrome/answer/3517349?hl=en
+
+Known Limits
+------
+
+- DOES NOT WORK WITH HTTPS: Google does not pass HTTPS traffic with DCP. The only way to do that is do a MITM by yourself, which is obviously terrible.
+- Only works with Python 2.7.9+: I need ```SSLContext```. Especially when you are using OSX, you need to get the latest Python by yourself. More about it: https://stackoverflow.com/questions/28228214/ssl-module-object-has-no-attribute-sslcontext 
+- Could not working with large files.
+- Your network admin can FORCE you to use the HTTP version of this proxy, so they can look into your traffic. I am giving an warning if that happens, but this code is NOT tested to make sure it is safe from attacks including but not limited to SSLStrip. Use at your own risk.
+
+Usage
+------
+
+    python proxy.py (-p 8080) (-m HTTPS) (-i 127.0.0.1)
+    
+    -p: Default: 8080
+        The port this proxy is listening on.
+    
+    -m: Default: HTTPS
+        The mode, HTTPS or HTTP.
+    
+    -i: Default: ''
+        The IP address this proxy is listening on.
+
+
+Requirement
+------
+
+- Python 2.7.9+
+- ```ssl``` package, as in openssl
+
+Author
+-----
+Beining, http://www.cnbeining.com/ .
+
+The header algorithm comes from datacompressionproxy, https://code.google.com/p/datacompressionproxy/ , License: Apache 2.0. Author: jerzyglowacki, http://www.niute.ch/ .
+
+The main part comes from Sogou Proxy, http://xiaoxia.org/2011/11/14/update-sogou-proxy-program-with-https-support/ . Author: Xiaoxia, http://xiaoxia.org .
+
+License
+-----
+GPLv2.
+
+This program is provided **as is**, with absolutely no warranty.
---
+++ google.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  Beining --<cnbeining#gmail.com>
+# Purpose: A Chrome DCP handler.
+# Created: 07/15/2015
+
+#Original copyright info:
+#Author:  Xiaoxia
+#Contact: xiaoxia@xiaoxia.org
+#Website: xiaoxia.org
+
+
+import sys
+import argparse
+
+from threading import Thread, Lock
+from struct import unpack
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from httplib import HTTPResponse, HTTPSConnection
+from SocketServer import ThreadingMixIn
+import socket, os, select
+import time, sys, random
+import threading
+
+import select
+import socket
+import ssl
+
+import socket
+import urllib2
+
+# Minimize Memory Usage
+threading.stack_size(128*1024)
+
+
+BufferSize = 8192
+RemoteTimeout = 15
+
+from hashlib import md5
+
+global PROXY_MODE
+PROXY_MODE = 'HTTPS'
+
+#----------------------------------------------------------------------
+def get_long_int():
+    """None->int
+    get a looooooong integer."""
+    return str(random.randint(100000000, 999999999))
+
+#----------------------------------------------------------------------
+def get_google_header():
+    """None->str
+    As in https://github.com/cnbeining/datacompressionproxy/blob/master/background.js#L10-L18 .
+    P.S: This repo is a fork of the original one on google code.
+    """
+    authValue = 'ac4500dd3b7579186c1b0620614fdb1f7d61f944'
+    timestamp = str(int(time.time()))
+    return 'ps=' + timestamp + '-' + get_long_int() + '-' + get_long_int() + '-' + get_long_int() + ', sid=' + md5((timestamp + authValue + timestamp).encode('utf-8')).hexdigest() + ', b=2403, p=61, c=win'
+
+#----------------------------------------------------------------------
+def check_if_ssl():
+    """None->str
+    Check whether DCP should use HTTPS.
+    As in https://support.google.com/chrome/answer/3517349?hl=en"""
+    response = urllib2.urlopen('http://check.googlezip.net/connect')
+    if response.getcode() is 200 and 'OK' in response.read():
+        print('INFO: Running in HTTPS mode.')
+        return 'HTTPS'
+    else:
+        print('WARNING: Running in HTTP mode, your network admin can see your traffic!')
+        return 'HTTP'
+
+class Handler(BaseHTTPRequestHandler):
+    remote = None
+
+    # Ignore Connection Failure
+    def handle(self):
+        try:
+            BaseHTTPRequestHandler.handle(self)
+        except socket.error: pass
+    def finish(self):
+        try:
+            BaseHTTPRequestHandler.finish(self)
+        except socket.error: pass
+
+    def sogouProxy(self):
+        if self.remote is None or self.lastHost != self.headers["Host"]:
+            if PROXY_MODE == 'HTTPS':
+                context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+                context.verify_mode = ssl.CERT_REQUIRED
+                context.check_hostname = True
+                context.load_default_certs()
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(RemoteTimeout)
+                self.remote = context.wrap_socket(s, server_hostname='proxy.googlezip.net')
+                self.remote.connect(('proxy.googlezip.net', 443))
+            else:  #HTTP
+                self.remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.remote.settimeout(RemoteTimeout)
+                self.remote.connect(("compress.googlezip.net", 80))
+        self.remote.sendall(self.requestline.encode('ascii') + b"\r\n")
+        # Add Verification Tags
+        self.headers["Chrome-Proxy"] = get_google_header()
+        headerstr = str(self.headers).replace("\r\n", "\n").replace("\n", "\r\n")
+        self.remote.sendall(headerstr.encode('ascii') + b"\r\n")
+        # Send Post data
+        if self.command == 'POST':
+            self.remote.sendall(self.rfile.read(int(self.headers['Content-Length'])))
+        response = HTTPResponse(self.remote, method=self.command)
+        response.begin()
+
+        # Reply to the browser
+        status = "HTTP/1.1 " + str(response.status) + " " + response.reason
+        self.wfile.write(status.encode('ascii') + b'\r\n')
+        hlist = []
+        for line in response.msg.headers: # Fixed multiple values of a same name
+            if 'TRANSFER-ENCODING' not in line.upper():
+                hlist.append(line)
+        self.wfile.write("".join(hlist) + b'\r\n')
+
+        if self.command == "CONNECT":  # NO HTTPS, as Chrome DCP does not allow HTTPS traffic
+            return None
+        else:
+            while True:
+                response_data = response.read(BufferSize)
+                if not response_data: break
+                self.wfile.write(response_data)
+
+    do_POST = do_GET = do_CONNECT = sogouProxy
+
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer): 
+    address_family = socket.AF_INET6
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', default=8080)
+    parser.add_argument('-m', '--mode', default= 'HTTPS')
+    parser.add_argument('-i', '--ip', default= '')
+    args = vars(parser.parse_args())
+    if str(args['mode']).upper() == 'HTTP' or str(args['mode']).upper() == 'HTTPS':
+        PROXY_MODE = args['mode'].upper()
+    else:
+        PROXY_MODE = check_if_ssl()
+    server_address = (str(args['ip']), int(args['port']))
+    server = ThreadingHTTPServer(server_address, Handler)
+    proxy_host = "proxy.googlezip.net:443" if PROXY_MODE == 'HTTPS' else "compress.googlezip.net:80"
+    print('Proxy over %s.\nPlease set your browser\'s proxy to %s.' % (proxy_host, server_address))
+    try:
+        server.serve_forever()
+    except:
+        os._exit(1)
